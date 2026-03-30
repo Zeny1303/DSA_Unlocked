c7be919d13f06841f73b2f66c1f5bd8a0b8d2b4a
Sneha <147318067+Zeny1303@users.noreply.github.com>
2026-03-30 18:15:13 +0530

    Time: 5 ms (85.17%) | Memory: 13.1 MB (90.19%) - LeetSync

---
+++ Leetcode/207-course-schedule/course-schedule.py
@@ -0,0 +1,40 @@
+from collections import deque
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        n = numCourses
+        # directed graph
+        graph = [[] for _ in range(n)]
+        for u,v in prerequisites:
+            graph[v].append(u)
+        # indegree calculations
+        indegree =[0]*n
+        for i in range(n):
+            for x in graph[i]:
+                indegree[x]+=1
+        # queue
+        q=deque()
+        for i in range(n):
+            if indegree[i]==0:
+                q.append(i)
+        ans =[]
+        # kahn's algorithm 
+        while q:
+            f=q.popleft()
+            ans.append(f)
+            for x in graph[f]:
+                indegree[x]-=1
+                if indegree[x]==0:
+                    q.append(x)
+        if len(ans) != n:
+            return False
+        return True                        
+
+
+        
+        
+        
