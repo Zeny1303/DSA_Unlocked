b7ed2449143ca2c5788d504e126901d4fca190cf
Sneha <147318067+Zeny1303@users.noreply.github.com>
2026-04-15 16:10:06 +0530

    Time: 1266 ms (27.25%) | Memory: 0B (100.00%) - LeetSync

--- Leetcode/1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ Leetcode/1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,6 +1,6 @@
-SELECT 
-    eu.unique_id, 
-    e.name
-FROM Employees e
-LEFT JOIN EmployeeUNI eu 
-    ON e.id = eu.id;
+# Write your MySQL query statement below
+select eu.unique_id, e.name  
+from Employees e
+left join EmployeeUNI eu
+on e.id = eu.id ;
+
