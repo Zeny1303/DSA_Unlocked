298b36e729cd7cb287976323077f112d90fe116f
Sneha <147318067+Zeny1303@users.noreply.github.com>
2026-05-25 12:55:26 +0530

    Time: 83 ms (83.67%) | Memory: 15.8 MB (21.07%) - LeetSync

---
+++ Leetcode/76-minimum-window-substring/minimum-window-substring.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+
+        if not s or not t:
+            return ""
+
+        countT = {}
+        window = {}
+
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have = 0
+        need = len(countT)
+
+        res = [-1, -1]
+        resLen = float("inf")
+
+        left = 0
+
+        for right in range(len(s)):
+
+            c = s[right]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            # shrink window
+            while have == need:
+
+                # update result
+                if (right - left + 1) < resLen:
+                    res = [left, right]
+                    resLen = right - left + 1
+
+                # remove left character
+                window[s[left]] -= 1
+
+                if (s[left] in countT and
+                        window[s[left]] < countT[s[left]]):
+                    have -= 1
+
+                left += 1
+
+        left, right = res
+
+        return s[left:right + 1] if resLen != float("inf") else ""
