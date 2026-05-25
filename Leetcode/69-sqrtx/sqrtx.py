cdc8eae1e88d1c13666946cd14a7f7535f7bca33
Sneha <147318067+Zeny1303@users.noreply.github.com>
2026-05-25 12:48:28 +0530

    Time: 4 ms (43.32%) | Memory: 12.4 MB (18.23%) - LeetSync

--- Leetcode/69-sqrtx/sqrtx.py
+++ Leetcode/69-sqrtx/sqrtx.py
@@ -4,20 +4,24 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+
         if x < 2:
             return x
-        
-        left, right = 1, x // 2  # sqrt(x) is always ≤ x//2 for x >= 4
-        
+
+        left = 1
+        right = x
+
         while left <= right:
-            mid = left + (right - left) // 2
-            
+
+            mid = (left + right) // 2
+
             if mid * mid == x:
                 return mid
+
             elif mid * mid < x:
                 left = mid + 1
+
             else:
                 right = mid - 1
-        
-        return right  # right is the floor when loop ends
 
+        return right
