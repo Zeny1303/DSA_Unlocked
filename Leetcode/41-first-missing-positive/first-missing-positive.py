89ed0790aa74f7e078d65a1e84bef0093bb4fd6e
Sneha <147318067+Zeny1303@users.noreply.github.com>
2026-05-25 09:27:58 +0530

    Time: 67 ms (69.37%) | Memory: 20.1 MB (39.81%) - LeetSync

---
+++ Leetcode/41-first-missing-positive/first-missing-positive.py
@@ -0,0 +1,35 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        n = len(nums)
+
+        # Place each number at correct index
+        # 1 -> index 0
+        # 2 -> index 1
+        # x -> index x-1
+
+        for i in range(n):
+
+            while (
+                1 <= nums[i] <= n and
+                nums[nums[i] - 1] != nums[i]
+            ):
+
+                correct_index = nums[i] - 1
+
+                nums[i], nums[correct_index] = (
+                    nums[correct_index],
+                    nums[i]
+                )
+
+        # Find first missing positive
+        for i in range(n):
+
+            if nums[i] != i + 1:
+                return i + 1
+
+        return n + 1
