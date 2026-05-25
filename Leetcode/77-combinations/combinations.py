37054bc5fac09384bb3c7f7a86c7181aad25fef7
Sneha <147318067+Zeny1303@users.noreply.github.com>
2026-05-25 12:56:52 +0530

    Time: 255 ms (76.70%) | Memory: 131.2 MB (75.38%) - LeetSync

---
+++ Leetcode/77-combinations/combinations.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+
+        result = []
+
+        def backtrack(start, path):
+
+            # combination complete
+            if len(path) == k:
+                result.append(path[:])
+                return
+
+            for i in range(start, n + 1):
+
+                path.append(i)
+
+                backtrack(i + 1, path)
+
+                path.pop()
+
+        backtrack(1, [])
+
+        return result
