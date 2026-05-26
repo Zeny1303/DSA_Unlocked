3b2c734d547f9f41d37154b8619c01775cc56a65
Sneha <147318067+Zeny1303@users.noreply.github.com>
2026-05-26 22:39:29 +0530

    Time: 1304 ms (74.39%) | Memory: 30.1 MB (35.37%) - LeetSync

---
+++ Leetcode/327-count-of-range-sum/count-of-range-sum.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def countRangeSum(self, nums, lower, upper):
+        """
+        :type nums: List[int]
+        :type lower: int
+        :type upper: int
+        :rtype: int
+        """
+
+        prefix = [0]
+
+        for num in nums:
+            prefix.append(prefix[-1] + num)
+
+        def merge_sort(lo, hi):
+
+            if hi - lo <= 1:
+                return 0
+
+            mid = (lo + hi) // 2
+
+            count = merge_sort(lo, mid) + merge_sort(mid, hi)
+
+            j = k = mid
+
+            # Count valid range sums
+            for left in prefix[lo:mid]:
+
+                while k < hi and prefix[k] - left < lower:
+                    k += 1
+
+                while j < hi and prefix[j] - left <= upper:
+                    j += 1
+
+                count += (j - k)
+
+            # Merge sorted halves
+            prefix[lo:hi] = sorted(prefix[lo:hi])
+
+            return count
+
+        return merge_sort(0, len(prefix))
