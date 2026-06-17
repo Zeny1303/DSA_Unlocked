e41255a50f91a9dfa71f9693701ca42c13f3d241
Sneha <147318067+Zeny1303@users.noreply.github.com>
2026-06-17 21:39:46 +0530

    Time: 302 ms (100.00%) | Memory: 20.8 MB (54.17%) - LeetSync

---
+++ Leetcode/3939-process-string-with-special-operations-ii/process-string-with-special-operations-ii.py
@@ -0,0 +1,37 @@
+class Solution:
+    def processStr(self, s: str, k: int) -> str:
+        length = 0
+
+        # Compute final length
+        for ch in s:
+            if ch == '*':
+                length = max(0, length - 1)
+            elif ch == '#':
+                length *= 2
+            elif ch == '%':
+                pass
+            else:
+                length += 1
+
+        if k >= length:
+            return '.'
+
+        # Reverse simulation
+        for ch in reversed(s):
+            if ch == '*':
+                length += 1
+
+            elif ch == '#':
+                length //= 2
+                if k >= length:
+                    k -= length
+
+            elif ch == '%':
+                k = length - 1 - k
+
+            else:
+                length -= 1
+                if k == length:
+                    return ch
+
+        return '.'
